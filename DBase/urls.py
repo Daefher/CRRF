99ac3b1efec7c5aee85f647f10d99e21577e5830
Daniel Eduardo Flores Hernández <daefher@gmail.com>
2019-03-14 09:49:53 -0600

    Login y logout agregados

--- DBase/urls.py
+++ DBase/urls.py
@@ -1,16 +1,17 @@
-
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
 
-
-
+from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('libro/', include('libro.urls')),
+    path('users/', include('users.urls')),
+    path('',user_views.login_view, name='home'),
+
 ]
 
 urlpatterns += staticfiles_urlpatterns()
--- users/views.py
+++ users/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.models import User
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == "POST":
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect("libro:proyect-list")
+    else:
+        if request.user.is_authenticated:
+            return redirect("libro:proyect-list")
+        else:
+            form = AuthenticationForm(request)
+    return render(request, 'login.html', {'form':form})
+
+def logout_view(request):    
+    logout(request)
+    return redirect('users:login')
