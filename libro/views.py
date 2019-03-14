32b2860c07a802e4cfb4e2710f56fcc76f22fc38
Daniel Eduardo Flores Hernández <daefher@gmail.com>
2019-03-14 09:10:50 -0600

    agregada proteccion sin login

--- libro/views.py
+++ libro/views.py
@@ -6,6 +6,10 @@ from django.urls import reverse, reverse_lazy
 from django.views import View
 from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib import messages
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -15,11 +19,11 @@ from django.views.generic import (
     DeleteView
 )
 
-
-class proyect_create_view(CreateView):
+class proyect_create_view(LoginRequiredMixin,CreateView):
     template_name = 'agregar_proyecto.html'
     form_class = Proyect_model_form
-    queryset = proyect.objects.all()    
+    queryset = proyect.objects.all() 
+    login_url = '/users/login/'   
 
     def post(self,request, *args, **kwargs):
         form = Proyect_model_form(request.POST)        
@@ -34,19 +38,33 @@ class proyect_create_view(CreateView):
         context = {"form":form}
         return render(request, self.template_name,context)
     
-
-class proyect_update_view(SuccessMessageMixin,UpdateView):
+class proyect_update_view(LoginRequiredMixin,SuccessMessageMixin,UpdateView):
     template_name = 'agregar_proyecto.html'
     form_class = Proyect_model_form
-    queryset = proyect.objects.all()  
+    login_url = '/users/login/'
+    # queryset = proyect.objects.all()  
     success_message = "Registro actualizado exitosamente"
 
     def get_object(self):
         id_ = self.kwargs.get('id')
         return get_object_or_404(proyect, id=id_)
+    
+    def post(self,request, *args, **kwargs):
+        form = Proyect_model_form(request.POST)        
+        if form.is_valid():
+            print(request.user)
+            form.instance.usuario = request.user
+            folio_inicial = form.data['folio_inicial']
+            form.instance.folio_final = int(folio_inicial) + (int(form.data['no_formatos'])-1)            
+            messages.success(request, "Registro creado correctamente")
+            form.save()
+            return redirect(reverse("libro:proyect-detail", kwargs={"id":form.instance.id} ) )                              
+        context = {"form":form}
+        return render(request, self.template_name,context)
   
-class proyect_list_view(ListView):
-    template_name = 'lista_proyectos.html'    
+class proyect_list_view(LoginRequiredMixin,ListView):
+    template_name = 'lista_proyectos.html' 
+    login_url = '/users/login/'
 
     def get_queryset(self):
         query = self.request.GET.get('q')        
@@ -59,23 +77,24 @@ class proyect_list_view(ListView):
         query = proyect.objects.all()         
         return query
 
-class proyect_detail_view(DetailView):
+class proyect_detail_view(LoginRequiredMixin,DetailView):
     template_name = 'proyecto_detalles.html'
+    login_url = '/users/login/'
     #queryset = proyect.objects.all()
 
     def get_object(self):
         id_ = self.kwargs.get('id')
         return get_object_or_404(proyect, id=id_)  
 
-class proyect_delete_view(DeleteView):   
+class proyect_delete_view(LoginRequiredMixin,DeleteView):   
     model = proyect
     success_url = reverse_lazy('libro:proyect-list')
+    login_url = '/users/login/'
     
     def get_object(self):
         id_ = self.kwargs.get('id')
         return get_object_or_404(proyect, id=id_)
 
-class proyect_search_view(ListView):
-    template_name = 'lista_proyectos.html'    
+
 
    
